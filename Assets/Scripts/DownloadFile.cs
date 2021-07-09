207a8be7a4752c3ae7ec0820f8b1a052050946b4
xunyou <zjqtsw@163.com>
2021-07-09 16:13:43 +0800

    1

---
+++ Assets/Scripts/DownloadFile.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Net;
+
+public class DownloadFile
+{
+    public string Url;
+    public DownloadFile(string url)
+    {
+        Url = url;
+    }
+    public long GetFileSize()
+    {
+        HttpWebRequest request;
+        HttpWebResponse response;
+        try
+        {
+            request = (HttpWebRequest)HttpWebRequest.CreateHttp(new Uri(Url));
+            request.Method = "HEAD";
+            response = (HttpWebResponse)request.GetResponse();
+            // 获得文件长度
+            long contentLength = response.ContentLength;
+
+            response.Close();
+            request.Abort();
+
+            return contentLength;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public void GetFileSizeAsyn(Action<long> onTrigger = null)
+    {
+
+    }
+
+
+
+}
