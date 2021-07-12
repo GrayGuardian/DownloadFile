395b85038e00550a8ac9be1593cf13ab97d46a3a
xunyou <zjqtsw@163.com>
2021-07-12 17:52:01 +0800

    文件尺寸获取；

--- Assets/Scripts/DownloadFile.cs
+++ Assets/Scripts/DownloadFile.cs
@@ -1,14 +1,30 @@
 ﻿
 using System;
 using System.Net;
+using System.Threading;
 
 public class DownloadFile
 {
+    /// <summary>
+    /// 主线程
+    /// </summary>
+    private SynchronizationContext _mainThread;
+
     public string Url;
     public DownloadFile(string url)
     {
+        // 获取主线程
+        _mainThread = SynchronizationContext.Current;
+
+        // 突破Http协议的并发连接数限制
+        System.Net.ServicePointManager.DefaultConnectionLimit = 512;
+
         Url = url;
     }
+    /// <summary>
+    /// 查询文件大小
+    /// </summary>
+    /// <returns></returns>
     public long GetFileSize()
     {
         HttpWebRequest request;
@@ -31,12 +47,50 @@ public class DownloadFile
             throw;
         }
     }
-
+    /// <summary>
+    /// 异步查询文件大小
+    /// </summary>
+    /// <param name="onTrigger"></param>
     public void GetFileSizeAsyn(Action<long> onTrigger = null)
     {
+        ThreadStart threadStart = new ThreadStart(() =>
+        {
+            PostMainThreadAction<long>(onTrigger, GetFileSize());
+        });
 
+        Thread thread = new Thread(threadStart);
+        thread.Start();
     }
 
-
+    /// <summary>
+    /// 通知主线程回调
+    /// </summary>
+    private void PostMainThreadAction(Action action)
+    {
+        _mainThread.Post(new SendOrPostCallback((o) =>
+        {
+            Action e = (Action)o.GetType().GetProperty("action").GetValue(o);
+            if (e != null) e();
+        }), new { action = action });
+    }
+    private void PostMainThreadAction<T>(Action<T> action, T arg1)
+    {
+        _mainThread.Post(new SendOrPostCallback((o) =>
+        {
+            Action<T> e = (Action<T>)o.GetType().GetProperty("action").GetValue(o);
+            T t1 = (T)o.GetType().GetProperty("arg1").GetValue(o);
+            if (e != null) e(t1);
+        }), new { action = action, arg1 = arg1 });
+    }
+    public void PostMainThreadAction<T1, T2>(Action<T1, T2> action, T1 arg1, T2 arg2)
+    {
+        _mainThread.Post(new SendOrPostCallback((o) =>
+        {
+            Action<T1, T2> e = (Action<T1, T2>)o.GetType().GetProperty("action").GetValue(o);
+            T1 t1 = (T1)o.GetType().GetProperty("arg1").GetValue(o);
+            T2 t2 = (T2)o.GetType().GetProperty("arg2").GetValue(o);
+            if (e != null) e(t1, t2);
+        }), new { action = action, arg1 = arg1, arg2 = arg2 });
+    }
 
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    private DownloadFile _downloadFile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var url = "https://dlc2.pconline.com.cn/filedown_1117483_12749837/KuVu8l3Y/pconline1552198052014.zip";
+
+        _downloadFile = new DownloadFile(url);
+
+        _downloadFile.GetFileSizeAsyn((size) =>
+        {
+            UnityEngine.Debug.LogFormat("异步读取文件尺寸：{0}", size);
+        });
+
+        UnityEngine.Debug.LogFormat("同步读取文件尺寸：{0}", _downloadFile.GetFileSize());
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
